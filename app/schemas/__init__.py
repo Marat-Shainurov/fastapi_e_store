baf7c1196f2d4b722745b1b1cfba06eeab1dc24e
Marat-Shainurov <m_shainurov@mail.ru>
2023-09-19 18:02:23 +0300

    adds the Token schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .users import UserBase, UserCreate, UserInDB
+from .tokens import Token, TokenData
 
-__all__ = ['UserBase', 'UserCreate', 'UserInDB']
+__all__ = ['UserBase', 'UserCreate', 'UserInDB', 'Token', 'TokenData']
