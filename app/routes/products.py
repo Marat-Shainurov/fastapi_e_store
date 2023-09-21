4384118fa36060ceda438dcb8754199d70ba8306
Marat-Shainurov <m_shainurov@mail.ru>
2023-09-21 14:48:45 +0300

    creates the products endpoints, adds the router to the app

---
+++ app/routes/products.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from app.database.db import get_db
+from app.schemas import UserInDB, ProductBase, ProductCreate, ProductInDb
+from app.services import add_product, get_products, get_product, put_product, destroy_product
+from app.services import get_current_active_user
+
+router = APIRouter(
+    prefix='/products',
+    tags=['products'],
+)
+
+
+@router.post("/", response_model=ProductInDb, status_code=status.HTTP_201_CREATED)
+async def create_product(
+        product: ProductCreate, db: Session = Depends(get_db), current_user: UserInDB = Depends(get_current_active_user)
+):
+    return add_product(db=db, product=product, current_user=current_user)
+
+
+@router.get("/")
+async def read_products(current_user: UserInDB = Depends(get_current_active_user), db: Session = Depends(get_db)):
+    return get_products(db=db)
+
+
+@router.get("/{product_id}")
+async def retrieve_product(product_id: int, current_user: UserInDB = Depends(get_current_active_user),
+                           db: Session = Depends(get_db)):
+    return get_product(db=db, product_id=product_id)
+
+
+@router.put("/{product_id}")
+async def update_product(product: ProductBase, product_id: int,
+                         current_user: UserInDB = Depends(get_current_active_user), db: Session = Depends(get_db)):
+    return put_product(product=product, product_id=product_id, db=db)
+
+
+@router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_product(product_id: int, db: Session = Depends(get_db),
+                         current_user: UserInDB = Depends(get_current_active_user)):
+    return destroy_product(product_id=product_id, db=db)
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 
 from app.database.config import SessionLocal
 from app.routes.users import router as users_router
+from app.routes.products import router as products_router
 
 app = FastAPI()
 
 app.include_router(users_router)
+app.include_router(products_router)
