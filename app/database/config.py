eff4578d2716758f7b83aeca99478274b2ae586f
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-01 14:48:34 +0300

    adds the testing db configuration

--- app/database/config.py
+++ app/database/config.py
@@ -2,7 +2,12 @@ from sqlalchemy import Column, Table, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Benzokolon1@localhost:5432/fastapi_store"
+TESTING = True
+
+TEST_DATABASE_URL = "postgresql://postgres:Benzokolon1@localhost:5432/tests_fastapi_store"
+PRODUCTION_DATABASE_URL = "postgresql://postgres:Benzokolon1@localhost:5432/fastapi_store"
+
+SQLALCHEMY_DATABASE_URL = TEST_DATABASE_URL if TESTING else PRODUCTION_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
