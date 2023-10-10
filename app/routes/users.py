0b5875ba0a8bc85d7abf28b7e198360d7bc35aad
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-10 14:28:06 +0300

    updates the create_access_token() function (removes the odd DEFAULT_EXPIRE variable)

--- app/routes/users.py
+++ app/routes/users.py
@@ -30,11 +30,7 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"}
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username},
-        expires_delta=access_token_expires
-    )
+    access_token = create_access_token(data={"sub": user.username})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
--- app/services/tokens.py
+++ app/services/tokens.py
@@ -8,7 +8,6 @@ pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 SECRET_KEY = 'topsecretkey'
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRES_MINUTES = 30
-DEFAULT_EXPIRE = 15
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -19,12 +18,9 @@ def get_password_hashed(plain_password):
     return pwd_context.hash(plain_password)
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=DEFAULT_EXPIRE)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
     to_encode.update({"exp": expire})
     encode_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
     return encode_jwt
