8941ee976f7c8fc0181fc966cc60fdf88d557417
Marat-Shainurov <m_shainurov@mail.ru>
2023-10-11 14:21:57 +0300

    sets db envs

--- app/database/config.py
+++ app/database/config.py
@@ -1,11 +1,18 @@
+import os
+
 from sqlalchemy import Column, Table, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TESTING = True
 
-TEST_DATABASE_URL = "postgresql://postgres:Benzokolon1@localhost:5432/tests_fastapi_store"
-PRODUCTION_DATABASE_URL = "postgresql://postgres:Benzokolon1@localhost:5432/fastapi_store"
+TEST_DATABASE_URL = \
+    f"postgresql://postgres:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}:5432/tests_fastapi_store"
+PRODUCTION_DATABASE_URL = \
+    f"postgresql://postgres:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}:5432/fastapi_store"
 
 SQLALCHEMY_DATABASE_URL = TEST_DATABASE_URL if TESTING else PRODUCTION_DATABASE_URL
 
